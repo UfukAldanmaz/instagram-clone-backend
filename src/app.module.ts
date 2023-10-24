ab134e359f476de39d1634e63f9fb4dec040c6dd
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2023-10-24 11:02:56 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,13 @@
 import 'reflect-metadata';
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import * as express from 'express';
-import { join } from 'path';
 
 // Modules
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { PostController } from './post/post.controller';
-import { PostService } from './post/post.service';
 import { PostModule } from './post/post.module';
-import { ConfigModule } from '@nestjs/config';
 import { FollowingModule } from './following/following.module';
 
 export const TypeOrmConfig = TypeOrmModule.forRootAsync({
